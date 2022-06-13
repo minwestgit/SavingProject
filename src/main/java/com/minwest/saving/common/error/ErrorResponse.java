f1ad0716ce65266e1d9fee46028c6af07108a3f4
minwestgit <minwest61@gmail.com>
2022-06-13 21:59:56 +0900

    Error Response 객체 개발

---
+++ src/main/java/com/minwest/saving/common/error/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.minwest.saving.common.error;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ErrorResponse {
+    private final String code;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder().code(errorCode.name()).message(errorCode.getMessage()).build());
+    }
+}
